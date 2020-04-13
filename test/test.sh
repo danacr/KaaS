f274d59dcd421adfc6f83aabeb237a74ec585831
danacr <dan@acristinii.com>
2020-04-13 12:31:48 +0200

    Support for 1.18.1

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,6 @@
 
 key=$(base64 < key.asc)
-json="{\"version\":\"v1.17.4\",\"pubkey\":\"$key\"}"
+json="{\"version\":\"v1.18.1\",\"pubkey\":\"$key\"}"
 
 curl --header "Content-Type: application/json" \
   --request POST \
